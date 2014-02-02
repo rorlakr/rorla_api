ae794506a977e492f50d6878463a4edac58b7cc7
Hyo Seong Choi <rorlab@gmail.com>
2014-02-02 18:43:05 +0900

    secret_token.rb 업데이트함

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -10,10 +10,11 @@
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
 
-# Although this is not needed for an api-only application, rails4 
-# requires secret_key_base or secret_toke to be defined, otherwise an 
+# Although this is not needed for an api-only application, rails4
+# requires secret_key_base or secret_toke to be defined, otherwise an
 # error is raised.
 # Using secret_token for rails3 compatibility. Change to secret_key_base
 # to avoid deprecation warning.
 # Can be safely removed in a rails3 api-only application.
-RorlaApi::Application.config.secret_token = '77ff13eb12048b2b2e3e5f374d1ee113da87952dc59846d14dd18a26a431dd26a0d869f266bfc163b6f449bd31be8e7b8d4c5c614681b69e60cde8145a3e15b9'
+RorlaApi::Application.config.secret_key_base = '77ff13eb12048b2b2e3e5f374d1ee113da87952dc59846d14dd18a26a431dd26a0d869f266bfc163b6f449bd31be8e7b8d4c5c614681b69e60cde8145a3e15b9'
+
