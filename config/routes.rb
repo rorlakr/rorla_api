168c971e0f005c6cf3131ae228cda1da498d6095
Hyo Seong Choi <rorlab@gmail.com>
2014-02-02 18:22:08 +0900

    users 리소스 라우팅 추가

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ RorlaApi::Application.routes.draw do
                sign_out: 'logout'
              },
              :controllers => { :sessions => "users/sessions" }
+  resources :users, except: [ :new, :edit ]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
