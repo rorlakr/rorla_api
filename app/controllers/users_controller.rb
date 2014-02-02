f64d31b7af092ce462600f31417cd41f9b77ce49
Hyo Seong Choi <rorlab@gmail.com>
2014-02-02 18:28:17 +0900

    users#create 버그 교정

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      render json: user
+      render json: @user
     else
       render json: @user.errors, status: :bad_request
     end
@@ -36,6 +36,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:name)
+    params.require(:user).permit(:email, :password)
   end
 end
