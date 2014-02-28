3bdf88e97f86ecf848dc133c0283b80ed28df564
Majestin <leekorea09@gmail.com>
2014-02-28 14:48:51 +0900

    Plaza Spec 추가

--- spec/models/plaza_spec.rb
+++ spec/models/plaza_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
-describe Plaza do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Plaza do 
+  describe "모델 객체의 생성" do
+    it "> 유효한 데이터로 생성한 Plaza 객체는 유효하다."  
+    it "> postit_type이 없으면 유효하지 않다."
+  end
+  describe "유효성 검증"
+  describe "관계선언 검증" do
+    it "> 한 개의 Plaza 객체는 여러개의 Post 객체들을 가질 수 있다."
+  end
+  describe "스코프 및 클래스 메소드 검증"
+  describe "인스턴스 메소드 검증"
 end
