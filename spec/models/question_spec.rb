bad89b2b9f84428644d5caf0088188886e8fda59
plaredspear <plaredspear@gmail.com>
2014-03-07 20:36:06 +0900

    Question Spec 초기화

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
 describe Question do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "모델 객체의 생성"
+  describe "유효성 검증"
+  describe "관계선언 검증"
+  describe "스코프 및 클래스 메소드 검증"
+  describe "인스턴스 메소드 검증"
 end
