d9cce5d578fb16e10551f80c12f310d9b99bb4f1
plaredspear <plaredspear@gmail.com>
2014-03-16 16:05:52 +0900

    Answer Model(content) 생성 & FactoryGirl, Spec 초기화

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140309064747) do
+ActiveRecord::Schema.define(version: 20140316065249) do
+
+  create_table "answers", force: true do |t|
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "auth_tokens", force: true do |t|
     t.integer  "user_id"
---
+++ spec/models/answer_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe Answer do
+  describe "모델 객체의 생성"
+  describe "유효성 검증"
+  describe "관계선언 검증"
+  describe "스코프 및 클래스 메소드 검증"
+  describe "인스턴스 메소드 검증"
+end
