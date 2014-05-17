0354ed5908353f7b4767e088f969d043ce35846b
Shin Jaehun <gunsmoke.shin@gmail.com>
2014-05-17 20:47:13 +0900

    Comment Controller Spec 작성

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -3,26 +3,23 @@ require 'spec_helper'
 describe CommentsController do
 
   let(:post_create) { create(:post) }
-  #let(:comment) {  create(:comment_to_post, commentable: post_create)  }
-  let(:comment) {  create(:comment, commentable: post_create)  }
+  let(:comment) {  create(:comment_to_post, commentable: post_create)  }
   let(:valid_attributes) { attributes_for(:comment) }
   let(:invalid_attributes) { attributes_for(:comment, content: nil) }
 
   describe "GET #index" do
     it "> 모든 Comment들을 @comments에 할당한다." do
-      #comments = [comment, create(:comment_to_post, commentable: post_create)]
-      comments = [comment, create(:comment, commentable: post_create)]
-      get :index
-      expect(assigns(:comments)).to match_array eq(comments)
+      comments = [comment, create(:comment_to_post, commentable: post_create)]
+      get :index, post_id: post_create.id
+      expect(assigns(:post)).to eq(post_create)
+      expect(assigns(:comments)).to match_array(comments)
     end
   end
 
   describe "GET #show" do
     it "> 요청한 Comment로 instance 변수를 할당한다." do
- 
-#       get :show, post_id: post.id, id: comment
-     get :show, post_create_id: post_create.id, id: comment
-      expect(assigns(:post)).to eq(post)
+      get :show, post_id: post_create.id, id: comment
+      expect(assigns(:post)).to eq(post_create)
       expect(assigns(:comment)).to eq comment
     end 
   end
@@ -30,7 +27,7 @@ describe CommentsController do
   describe "POST #create" do
     context "1) params가 유효할 때" do
       def do_post
-        post :create, comment: valid_attributes
+        post :create, post_id: post_create.id, comment: valid_attributes
       end
 
       it "> 새로운 Comment를 생성한다." do
@@ -48,7 +45,7 @@ describe CommentsController do
 
     context "2) params가 유효하지 않을 때" do
       def do_post
-        post :create, comment: invalid_attributes
+        post :create, post_id: post_create.id, comment: invalid_attributes
       end
 
       it "> 새로운 Comment를 생성하지 않는다." do
@@ -69,7 +66,7 @@ describe CommentsController do
   describe 'PATCH #update' do
     context "1) params가 유효할 때" do
       def do_patch
-        patch :update, post_id: post.id, id: comment,
+        patch :update, post_id: post_create.id, id: comment,
           comment: attributes_for(:comment, content: "Answer content changed")
       end
 
@@ -82,14 +79,14 @@ describe CommentsController do
       it "> 요청한 Post와 Comment로 instance 변수를 할당한다." do
         do_patch
         comment.reload
-        expect(assigns(:post)).to eq(post)
+        expect(assigns(:post)).to eq(post_create)
         expect(assigns(:comment)).to eq comment
       end
     end
 
     context "2) params가 유효하지 않을 때" do
       def do_patch
-        patch :update, post_id: post.id, id: comment, comment: invalid_attributes
+        patch :update, post_id: post_create.id, id: comment, comment: invalid_attributes
       end
 
       it "> 요청한 Comment를 업데이트하지 않는다." do
@@ -101,7 +98,7 @@ describe CommentsController do
       it "> 요청한 Post와 Comment로 instance 변수를 할당한다." do
         do_patch
         comment.reload
-        expect(assigns(:post)).to eq(post)
+        expect(assigns(:post)).to eq(post_create)
         expect(assigns(:comment)).to eq comment
       end
     end
@@ -109,11 +106,11 @@ describe CommentsController do
 
   describe 'DELETE #destroy' do
     before(:each) do
-      @comment = create(:comment, commentable: post)
+      @comment = create(:comment, commentable: post_create)
     end
 
     def do_delete
-      delete :destroy, post_id: post.id, id: @comment
+      delete :destroy, post_id: post_create.id, id: @comment
     end
 
     it "> 요청한 Comment를 삭제한다." do
@@ -124,7 +121,7 @@ describe CommentsController do
 
     it "> 요청한 Post와 삭제된 Comment로 instance 변수를 할당한다." do
       do_delete
-      expect(assigns(:post)).to eq(post)
+      expect(assigns(:post)).to eq(post_create)
       expect(assigns(:comment)).to eq(@comment)
     end
   end
